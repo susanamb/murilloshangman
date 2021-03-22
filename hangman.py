640942191ce06dce96979e12203733fc49336cf0
Susana Murillo <susana.murillo@uabc.edu.mx>
2021-03-22 13:03:28 -0700

    install pipenv and colorama

--- hangman.py
+++ hangman.py
@@ -1,5 +1,9 @@
 from words import palabras
+import colorama
+from colorama import Fore,init
+init()
 import random
+colors = list(vars(colorama.Fore).values())
 
 print("Welcome to the Hangman game, let's play (;")
 print("Guess the word or phrase\n")
@@ -10,7 +14,11 @@ def good_word(words):
         word = random.choice(words)
     return word
 
-for i in good_word(palabras):
-    print(" _ ",end="")
+word = good_word(palabras)
 
+#print (word)
+for i in word:
+    colored_chars = [random.choice(colors) + " _ "]
+    print(" ".join(colored_chars),end="")
+#print(" - p"*len(word))
 print("")
